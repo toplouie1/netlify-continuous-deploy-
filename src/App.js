159534b6f53b52e44dce0dbcf0e09a5387ba0e54
Nima sherpa <Toplouie78@gmail.com>
2021-12-03 12:15:01 -0500

    img

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
+				<img src={logo} className="App-logo" alt="logo" />
+				<img src={logo} className="App-logo" alt="logo" />
+
 				<p>This is a deployed page</p>
 				<a
 					className="App-link"
